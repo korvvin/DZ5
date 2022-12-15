8f6428c38a6ed957eb53faf750bae28afbc87429
zykov <zykov.da01@gmail.com>
2022-12-15 23:32:40 +0300

    DZ5ред.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        //task1
+    //task1
         String firstName ="Семён";
         String middleName = "Семёнович";
         String lastName = "Иванов";
         String fullName = lastName+' '+firstName+' '+middleName;
         System.out.println(fullName);
-        //task2
+    //task2
         String upName = fullName.toUpperCase();
         System.out.println(upName);
-        //task3
+    //task3
         fullName = fullName.replace('ё','е');
         System.out.println(fullName);
     }
